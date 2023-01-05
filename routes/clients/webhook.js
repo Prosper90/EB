06b7bfa5b9f8d47d59b750ce2a858c62210d40dd
Prosper <nkonyeprospero@gmail.com>
2023-01-05 20:32:56 +0100

    still testing

--- routes/clients/webhook.js
+++ routes/clients/webhook.js
@@ -75,15 +75,10 @@ router.get('/', async (req, res) => {
 
 router.post("/flw-webhook", (req, res) => {
     // If you specified a secret hash, check for the signature
-    const secretHash = process.env.FLW_SECRET_HASH;
-    const signature = req.headers["verif-hash"];
-    if (!signature || (signature !== secretHash)) {
-        // This request isn't from Flutterwave; discard
-        res.status(401).end();
-    }
     const payload = req.body;
     // It's a good idea to log all received events.
-    log(payload);
+    console.log("called webhook and it worked so getting ready")
+    console.log(payload);
     // Do something (that doesn't take too long) with the payload
     res.status(200).end()
 });
